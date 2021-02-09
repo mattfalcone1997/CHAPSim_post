0c0d6c2a2eb48da613b1a59fabb9c0266013983d
Matthew Falcone <mfalcon1@sheffield.ac.uk>
2021-02-09 21:43:50 +0000

    update to use readthedocs

--- setup.py
+++ setup.py
@@ -5,28 +5,28 @@ if __name__ == "__main__":
     from numpy.distutils.core import Extension
     from Cython.Build import cythonize
 
-    ext_list = []
-    try:
-        f90_ext = Extension(name = 'CHAPSim_post.CHAPSim_post._f90_ext_base',
-                            sources = ["src/autocorr_parallel.f90"],
-                            extra_link_args=["-lgomp"],
-                            extra_f90_compile_args=['-O3','-fopenmp'])
-    except Exception as e:
-        print(e)
-    else:
-        ext_list.append(f90_ext)
+
+    f90_ext = Extension(name = 'CHAPSim_post.CHAPSim_post._f90_ext_base',
+                        sources = ["src/autocorr_parallel.f90"],
+                        extra_link_args=["-lgomp"],
+                        extra_f90_compile_args=['-O3','-fopenmp'])
+
     
     cy_ext = Extension(name = "CHAPSim_post.CHAPSim_post._cy_ext_base",
                     sources = ["src/autocorr_parallel.pyx"],
                     extra_compile_args = ["-fopenmp","-O3"],
                     extra_link_args = ["-fopenmp","-O3"])
-    ext_list.append(cy_ext)
             
-
-    config = Configuration(package_name='CHAPSim_post',
-                            description="Package containing post-processing routines for the CHAPSim DNS Solver",
-                            package_path="core/CHAPSim_post",
-                            ext_modules = cythonize(ext_list))
+    try:
+        config = Configuration(package_name='CHAPSim_post',
+                                description="Package containing post-processing routines for the CHAPSim DNS Solver",
+                                package_path="core/CHAPSim_post",
+                                ext_modules = cythonize([cy_ext,f90_ext]))
+    except Exception as e:
+        config = Configuration(package_name='CHAPSim_post',
+                                description="Package containing post-processing routines for the CHAPSim DNS Solver",
+                                package_path="core/CHAPSim_post",
+                                ext_modules = cythonize([cy_ext]))
 
     config.add_subpackage(subpackage_name='CHAPSim_post',
                         subpackage_path="core/CHAPSim_post/CHAPSim_post")
