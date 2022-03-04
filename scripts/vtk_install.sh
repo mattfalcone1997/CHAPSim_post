85fce76ad93bc71e0d80f84c9c8f422f5f98d351
Matthew Falcone <mfalcone1@sheffield.ac.uk>
2022-03-04 16:51:03 +0000

    Updated installation scripts

--- scripts/vtk_install.sh
+++ scripts/vtk_install.sh
@@ -79,12 +79,11 @@ LIB_PATH=$VTK_DEPS/build/install
 
 git clone --recursive -b v5.10.0-RC1 https://gitlab.kitware.com/paraview/paraview-superbuild.git
 mkdir -p build && cd build
-cmake -GNinja -DENABLE_osmesa=ON \
+cmake -GNinja -DENABLE_egl=ON \
                 -DENABLE_mpi=OFF \
                 -DENABLE_hdf5=ON \
                 -DENABLE_ffmpeg=ON \
                 -DENABLE_paraview=OFF \
-                -Dmesa_USE_SWR=ON  \
                 -DUSE_SYSTEM_python3=ON \
                 ../paraview-superbuild
 
