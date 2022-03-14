89f6b42080cdc05185f15a0827e811d5387b08fd
Matthew Falcone <mfalcone1@sheffield.ac.uk>
2022-03-14 17:42:26 +0000

    Update to integration

--- CHAPSim_post/_libs/integrate.pyx
+++ CHAPSim_post/_libs/integrate.pyx
@@ -147,7 +147,7 @@ def IntegrateTrapz(np.ndarray input_array,np.ndarray x, int axis=0, bint stagger
     cdef int i, j=0
 
     if dim == 1:
-        shape = np.ones(1)
+        shape = np.ones(1,dtype=int)
     else:
         shape = np.zeros(dim-1,dtype=int)
         for i in range(dim):
