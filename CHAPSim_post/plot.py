ed6e516cf21cfb63f445254ccfffa9ad04ca8a33
Matthew Falcone <mfalcone1@sheffield.ac.uk>
2022-04-18 09:07:14 +0100

    Addition of transforms to plot_contour

--- CHAPSim_post/plot.py
+++ CHAPSim_post/plot.py
@@ -682,7 +682,7 @@ def update_quiver_kw(quiver_kw,**kwargs):
 
 def update_line_kw(line_kw,override=False,**kwargs):
     if override:
-        return  _default_update_new('line_kw',line_kw,**kwargs)
+        return  _default_update_replace('line_kw',line_kw,**kwargs)
     else:
         return  _default_update_new('line_kw',line_kw,**kwargs)
 
