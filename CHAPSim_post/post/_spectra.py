ac316bf4d8c97804827ffa483d1b54e5156bfb7f
Matthew Falcone <mfalcone1@sheffield.ac.uk>
2022-04-21 15:02:33 +0100

    Test spectra phase average

--- CHAPSim_post/post/_spectra.py
+++ CHAPSim_post/post/_spectra.py
@@ -92,7 +92,7 @@ class _Spectra_base(Common):
         axis = fstruct.get_dim_from_axis(item)
         print(spec_array.shape,type(spec_array),self._comp)
         dk = np.diff(k_array)[0]
-        array =  np.fft.irfft(spec_array,axis=axis,norm='forward')*dk
+        array =  numpy_fft.irfft(spec_array,axis=axis,norm='forward')*dk
         
         if norm:
             array = array/array[0]
