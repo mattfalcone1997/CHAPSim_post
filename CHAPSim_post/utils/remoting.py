ea32bfe6a7e7794da92cc18bd3208338c77bfac8
Matthew Falcone <mfalcone1@sheffield.ac.uk>
2022-04-23 15:07:12 +0100

    Update to remote class

--- CHAPSim_post/utils/remoting.py
+++ CHAPSim_post/utils/remoting.py
@@ -10,8 +10,20 @@ class RemoteSSH:
         
         self._address = address
         
-            
+    def _user_ready(self):            
+        x = input(f"Are you ready to log in to ssh at {self._address} (y or n)?")
+        if x.lower() == 'n':
+            msg = "You are not ready"
+            raise RuntimeError(msg)
+        elif x.lower() == 'y':
+            pass
+        else:
+            print("You have given an invalid input.")
+            self._user_ready()
+                    
     def get_file(self,remote_file,local_file=None):
+        
+        self._user_ready()
         
         remote_cmd = self._address + ':' + remote_file 
         if local_file is None:
