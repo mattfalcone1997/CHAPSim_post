295311444b8661434ca66aca9c1ff635c72a656f
Matthew Falcone <mfalcone1@sheffield.ac.uk>
2022-04-23 18:41:41 +0100

    Update to temporal phase averaging

--- CHAPSim_post/post/_common.py
+++ CHAPSim_post/post/_common.py
@@ -241,7 +241,7 @@ class temporal_base(ABC):
     def _get_intersect(cls,times_list):
         _intersect = lambda times: [any(np.allclose(x,times)) for x in times_list[0]]
         
-        intersection = np.array( _intersect(times) for times in times_list[1:]).all(axis=0)
+        intersection = np.array( [_intersect(times) for times in times_list[1:]]).all(axis=0)
         print(intersection)
         return times_list[0][intersection]
         
