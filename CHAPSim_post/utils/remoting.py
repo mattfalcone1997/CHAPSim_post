909897e7d1aebeaebe5094125e84a0ae4800569e
Matthew Falcone <mfalcone1@sheffield.ac.uk>
2022-04-23 12:08:22 +0100

    Update to remote class

--- CHAPSim_post/utils/remoting.py
+++ CHAPSim_post/utils/remoting.py
@@ -1,7 +1,4 @@
-from imp import new_module
-from paramiko import SSHClient, rsakey, pkey, ssh_exception
-import getpass
-from scp import SCPClient, SCPException
+import subprocess
 import os
 
 class RemoteSSH:
@@ -11,62 +8,21 @@ class RemoteSSH:
             msg = "Issue with address"
             raise ValueError(msg)
         
-        self._hostname = user_host[-1]
-        self._user = user_host[0]
-        self.__password = None    
-        self.__keyfile = None
-        self.__pkey_password = None
+        self._address = address
         
-    def set_keyfile(self,keyfile):
-        if not os.path.isfile(keyfile):
-            msg = f"Keyfile specified {keyfile}, not found"
-            raise FileNotFoundError(msg)
-        
-        
-        self.__pkey_password = self._get_pkey_pword(keyfile)           
-        self.__keyfile = keyfile
-        
-    def _get_pkey_pword(self,keyfile,password=None):
-        try:
-            rsakey.RSAKey.from_private_key_file(open(keyfile,'r'),password=password)
-            return password
-        
-        except ssh_exception.PasswordRequiredException:
-            msg = f"Passphrase for key file {keyfile}"
-            password = getpass.getpass(msg)
-            return  self._get_pkey_pword(keyfile,password=password)
-        
-    def _get_password(self):
-        if self.__password is None:
-            name = "@".join([self._user,self._hostname])
-            self.__password = getpass.getpass("Password for %s:"%name)
             
-    def get_file(self,file_name,name=None):
-        self._get_password()
+    def get_file(self,remote_file,local_file=None):
         
-        client = SSHClient()
-        client.load_system_host_keys()
+        remote_cmd = self._address + ':' + remote_file 
+        if local_file is None:
+            local_file = os.path.basename(remote_file)
         
-        try:
-            client.connect(self._hostname,
-                        username=self._user,
-                        password=self.__password,
-                        key_filename=self.__keyfile,
-                        passphrase=self.__pkey_password)
-        except ssh_exception.AuthenticationException as e:
-            msg = "SSh client raised an exception. You may need to set private key file:\n"
-            raise ssh_exception.AuthenticationException(msg+e.args[0]) from None
+        cmd = ['scp',remote_cmd,local_file]
         
-        if name is None:
-            name = os.path.basename(file_name)
-            
-        scp = SCPClient(client.get_transport())
-        try:
-            scp.get(file_name,name)
-        except SCPException:
-            msg = f"SCP client raised an exception: {e.args[0]}"
-            raise SCPException from None
+        subprocess.run(cmd)
         
+
+
         
         
         
